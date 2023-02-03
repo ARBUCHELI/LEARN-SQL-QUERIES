23f19ba69c27c2e9eeed3103b077b7bb9daf8269
ARBUCHELI <radio_seattle@hotmail.com>
2023-02-03 17:47:56 -0500

    adding AS

--- queries.sql
+++ queries.sql
@@ -1,42 +1,59 @@
---CREATE TABLE movies (
---    id INTEGER,
---    name TEXT,
---    genre TEXT,
---    year INTEGER, 
---    imdb_rating REAL
---)
+-- *** INTRODUCTION *** --
 
---INSERT INTO movies (id, name, genre, year, imdb_rating)
---VALUES(1, 'Avatar', 'action', 2009, 7.9);
+/*CREATE TABLE movies (
+    id INTEGER,
+    name TEXT,
+    genre TEXT,
+    year INTEGER, 
+    imdb_rating REAL
+)/*
 
---INSERT INTO movies (id, name, genre, year, imdb_rating)
---VALUES(2, 'Jurassic World', 'action', 2015, 7.3);
+/*INSERT INTO movies (id, name, genre, year, imdb_rating)
+VALUES(1, 'Avatar', 'action', 2009, 7.9);
 
---INSERT INTO movies (id, name, genre, year, imdb_rating)
---VALUES(3, 'The Avengers', 'action', 2012, 8.1);
+INSERT INTO movies (id, name, genre, year, imdb_rating)
+VALUES(2, 'Jurassic World', 'action', 2015, 7.3);
 
---INSERT INTO movies (id, name, genre, year, imdb_rating)
---VALUES(4, 'The Dark Knight', 'action', 2008, 9.0);
+INSERT INTO movies (id, name, genre, year, imdb_rating)
+VALUES(3, 'The Avengers', 'action', 2012, 8.1);
 
---INSERT INTO movies (id, name, genre, year, imdb_rating)
---VALUES(5, 'Star Wars: Episode I - The Phantom Menace', 'action', 1999, 6.6);
+INSERT INTO movies (id, name, genre, year, imdb_rating)
+VALUES(4, 'The Dark Knight', 'action', 2008, 9.0);
 
---INSERT INTO movies (id, name, genre, year, imdb_rating)
---VALUES(6, 'Star Wars', 'action', 1977, 8.7);
+INSERT INTO movies (id, name, genre, year, imdb_rating)
+VALUES(5, 'Star Wars: Episode I - The Phantom Menace', 'action', 1999, 6.6);
 
---INSERT INTO movies (id, name, genre, year, imdb_rating)
---VALUES(7, 'Avengers: Age of Ultron', 'action', 2015, 7.9);
+INSERT INTO movies (id, name, genre, year, imdb_rating)
+VALUES(6, 'Star Wars', 'action', 1977, 8.7);
 
---INSERT INTO movies (id, name, genre, year, imdb_rating)
---VALUES(8, 'The Dark Knight Rises', 'action', 2012, 8.5);
+INSERT INTO movies (id, name, genre, year, imdb_rating)
+VALUES(7, 'Avengers: Age of Ultron', 'action', 2015, 7.9);
 
---INSERT INTO movies (id, name, genre, year, imdb_rating)
---VALUES(9, 'Pirates of the Caribbean: Dead Mans Chest', 'action', 2006, 7.3);
+INSERT INTO movies (id, name, genre, year, imdb_rating)
+VALUES(8, 'The Dark Knight Rises', 'action', 2012, 8.5);
 
---INSERT INTO movies (id, name, genre, year, imdb_rating)
---VALUES(10, 'Iron Man 3', 'action', 2013, 7.3);
+INSERT INTO movies (id, name, genre, year, imdb_rating)
+VALUES(9, 'Pirates of the Caribbean: Dead Mans Chest', 'action', 2006, 7.3);
 
-SELECT * FROM movies;
+INSERT INTO movies (id, name, genre, year, imdb_rating)
+VALUES(10, 'Iron Man 3', 'action', 2013, 7.3);*/
+
+/*SELECT * FROM movies;*/
+
+
+-- *** SELECT *** --
+/*SELECT name, genre, year
+FROM movies;*/
+
+
+-- *** AS *** --
+/*SELECT name AS 'Titles'
+FROM movies;
+
+SELECT imdb_rating AS 'IMDb'
+FROM movies;*/
+
+
 
 
 
